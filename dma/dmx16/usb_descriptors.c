f3c02b706a2a420a9a4b7945bb9f5798195db628
Jannis Achstetter <kripton@kripserver.net>
2021-01-30 10:23:34 +0100

    Update USB identifiers once again ;)

--- dma/dmx16/usb_descriptors.c
+++ dma/dmx16/usb_descriptors.c
@@ -129,10 +129,10 @@ uint8_t const *tud_descriptor_configuration_cb(uint8_t index) {
 char const *string_desc_arr[] =
 {
     (const char[]) {0x09, 0x04}, // 0: is supported language is English (0x0409)
-    "DE/FX5 Clone",              // 1: Manufacturer
-    "16Tx 00Rx",                 // 2: Product
-    "HEXADEC_000001",            // 3: Serials, should use chip ID
-    "DebugCDC"                   // 4: CDC interface name
+    "DE/FX5/U1 Clone",           // 1: Manufacturer
+    "16Tx 00Rx S",               // 2: Product
+    "RP2040_000001",             // 3: Serial, should use chip ID. TODO
+    "16Tx00RxS_000001"           // 4: CDC interface name
 };
 
 static uint16_t _desc_str[32];

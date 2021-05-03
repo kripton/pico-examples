a3472f55edc085331ac28ce4cd7b33001de25dd4
Jannis Achstetter <kripton@kripserver.net>
2021-05-03 23:23:19 +0200

    Convert flash/program to an example demonstrating how to persistently store values

--- flash/program/flash_program.c
+++ flash/program/flash_program.c
@@ -4,55 +4,50 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 #include "pico/stdlib.h"
 #include "hardware/flash.h"
 
-// We're going to erase and reprogram a region 256k from the start of flash.
-// Once done, we can access this at XIP_BASE + 256k.
-#define FLASH_TARGET_OFFSET (256 * 1024)
+// We're going to erase and reprogram a region 2044k from the start of flash.
+// Once done, we can access this at XIP_BASE + 2044k.
+#define FLASH_TARGET_OFFSET (2044 * 1024)
 
 const uint8_t *flash_target_contents = (const uint8_t *) (XIP_BASE + FLASH_TARGET_OFFSET);
 
-void print_buf(const uint8_t *buf, size_t len) {
-    for (size_t i = 0; i < len; ++i) {
-        printf("%02x", buf[i]);
-        if (i % 16 == 15)
-            printf("\n");
-        else
-            printf(" ");
-    }
-}
-
 int main() {
-    stdio_init_all();
-    uint8_t random_data[FLASH_PAGE_SIZE];
-    for (int i = 0; i < FLASH_PAGE_SIZE; ++i)
-        random_data[i] = rand() >> 16;
+    // Init the LED's GPIO so we can use it
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    gpio_put(PICO_DEFAULT_LED_PIN, 0);
 
-    printf("Generated random data:\n");
-    print_buf(random_data, FLASH_PAGE_SIZE);
+    uint8_t data_to_be_written[FLASH_SECTOR_SIZE];
 
-    // Note that a whole number of sectors must be erased at a time.
-    printf("\nErasing target region...\n");
-    flash_range_erase(FLASH_TARGET_OFFSET, FLASH_SECTOR_SIZE);
-    printf("Done. Read back target region:\n");
-    print_buf(flash_target_contents, FLASH_PAGE_SIZE);
+    // Read the target region
+    memcpy(data_to_be_written, flash_target_contents, FLASH_SECTOR_SIZE);
 
-    printf("\nProgramming target region...\n");
-    flash_range_program(FLASH_TARGET_OFFSET, random_data, FLASH_PAGE_SIZE);
-    printf("Done. Read back target region:\n");
-    print_buf(flash_target_contents, FLASH_PAGE_SIZE);
-
-    bool mismatch = false;
-    for (int i = 0; i < FLASH_PAGE_SIZE; ++i) {
-        if (random_data[i] != flash_target_contents[i])
-            mismatch = true;
+    if (data_to_be_written[0] > 10) {
+        data_to_be_written[0] = 1;
     }
-    if (mismatch)
-        printf("Programming failed!\n");
-    else
-        printf("Programming successful!\n");
+
+    // Blink the LED as often as the first byte tells us to
+    for (uint8_t i = 0; i < data_to_be_written[0]; i++) {
+        gpio_put(PICO_DEFAULT_LED_PIN, 1);
+        sleep_ms(250);
+        gpio_put(PICO_DEFAULT_LED_PIN, 0);
+        sleep_ms(250);
+    }
+
+    // Increase the first byte's value
+    data_to_be_written[0] = data_to_be_written[0] + 1;
+
+    // Erase the flash sector
+    // Note that a whole number of sectors must be erased at a time.
+    flash_range_erase(FLASH_TARGET_OFFSET, FLASH_SECTOR_SIZE);
+
+    // Program the flash sector with the new values
+    flash_range_program(FLASH_TARGET_OFFSET, data_to_be_written, FLASH_SECTOR_SIZE);
+
+    // Don't do anything else - wait for a reset
+    return 0;
 }
